a51e120569e3d4772560fa3255ec415e12ebf910
rhuanlunz <rhuan@C0V1L>
2026-05-15 01:00:13 -0300

    feat: update AuthMiddleware to return Response type and implement rate limiting for register and login routes

--- backend/app/Http/Middleware/AuthMiddleware.php
+++ backend/app/Http/Middleware/AuthMiddleware.php
@@ -5,11 +5,12 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Tymon\JWTAuth\Exceptions\UserNotDefinedException;
 
 class AuthMiddleware
 {
-    public function handle(Request $request, Closure $next): JsonResponse
+    public function handle(Request $request, Closure $next): JsonResponse | Response
     {
         try {
             auth()->userOrFail();
--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-Route::get('logout', [AuthController::class, 'logout']);
+Route::post('register', [AuthController::class, 'register'])
+    ->middleware(['throttle:register']);
+
+Route::post('login', [AuthController::class, 'login'])
+    ->middleware(['throttle:login']);
+
+Route::get('logout', [AuthController::class, 'logout'])
+    ->middleware(AuthMiddleware::class);
