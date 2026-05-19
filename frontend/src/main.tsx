6a939b7ff31f558707f0a549edd93ca14ea9bcd5
rhuanlunz <rhuan@C0V1L>
2026-05-19 11:00:59 -0300

    feat: added react-router package and configured front-end auth endpoints

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,27 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { BrowserRouter, Routes, Route } from "react-router"
+import ForgotPassword from "./Auth/ForgotPassword/ForgotPassword.tsx"
+import ResetPassword from "./Auth/ResetPassword/ResetPassword.tsx"
+import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/autenticacao">
+            <Route path="login" element={<App />} />
+            <Route path="cadastro" element={<App />} />
+            <Route path="esqueci-minha-senha" element={<ForgotPassword />} />
+            <Route path="redefinir-senha" element={<ResetPassword />} />
+          </Route>
+        
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>
 )
