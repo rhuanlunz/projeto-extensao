3d2e3a92204377416862754e6f7b6c8f4d90300e
Ulisses Castiglioni <ulissescastiglioni@gmail.com>
2026-05-21 20:21:42 -0300

    feat: implement sidebar according to architecture

---
+++ frontend/src/Resources/Resources.tsx
@@ -0,0 +1,14 @@
+import { Sidebar } from "@/shared/Sidebar/Sidebar";
+
+export function Resources() {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <main className="flex-1">
+        {/* Conteúdo futuro da listagem de recursos */}
+      </main>
+    </div>
+  );
+}
+
+export default Resources;
